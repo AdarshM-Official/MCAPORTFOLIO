bccc0c4685cb56c2942d230ef78fdfc30577b45a
AdarshM-Official <adarshm.off@gmail.com>
2025-08-14 16:49:00 +0530

    teachers page update

--- src/pages/teachers.js
+++ src/pages/teachers.js
@@ -1,8 +1,11 @@
+import Footer1_97SQaMJ8 from "../components/Footer1_97SQaMJ8";
+
 const Teachers = () => {
     return (
         <div>
             <h1>Teachers Page</h1>
             <p>This page will contain information about teachers.</p>
+            <Footer1_97SQaMJ8 />
         </div>
     );
 }
